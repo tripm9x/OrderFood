6a296fc8b002afdc684ed38df4ac69ba5ba77b41
Suusoft Phạm Minh Trí <tripmph05786@gmail.com>
2021-05-09 23:11:06 +0700

    add firebase cloud

--- app/src/main/java/com/volio/order_food/view/LoginFag.kt
+++ app/src/main/java/com/volio/order_food/view/LoginFag.kt
@@ -1,13 +1,35 @@
 package com.volio.order_food.view
 
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.volio.order_food.R
 import com.volio.order_food.base.BaseFrag
 import com.volio.order_food.util.setPreventDoubleClick
 import kotlinx.android.synthetic.main.fragment_login.*
 
-
+private const val TAG = "LoginFag"
 class LoginFag : BaseFrag(R.layout.fragment_login) {
+    val db = Firebase.firestore
+    val user = hashMapOf(
+        "user_name" to "Ada",
+        "user_phone" to "Lovelace",
+        "user_name" to 1815
+    )
     override fun onCreatedView() {
+        db.collection("users")
+            .whereEqualTo("user_phone","0392662642")
+            .get()
+            .addOnSuccessListener { result ->
+                Log.e(TAG, "onCreatedView: ${result.size()}" )
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
         btnRegister.setPreventDoubleClick {
             gotoFrag(R.id.loginFag,R.id.action_loginFag_to_registerFrag)
         }
--- app/src/main/java/com/volio/order_food/view/RegisterFrag.kt
+++ app/src/main/java/com/volio/order_food/view/RegisterFrag.kt
@@ -1,18 +1,40 @@
 package com.volio.order_food.view
 
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.volio.order_food.R
 import com.volio.order_food.base.BaseFrag
 import com.volio.order_food.util.setPreventDoubleClick
 import kotlinx.android.synthetic.main.fragment_register.*
 
+private const val TAG = "RegisterFrag"
 
 class RegisterFrag : BaseFrag(R.layout.fragment_register) {
+
+    val db = Firebase.firestore
+    val user = hashMapOf(
+        "user_name" to "Ada",
+        "user_phone" to "0392662642",
+        "password" to 12345678
+    )
+
     override fun onCreatedView() {
         btnBackRegister.setPreventDoubleClick {
             onBackPress()
         }
 
         btnRegister.setPreventDoubleClick {
+
+// Add a new document with a generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
             onBackPress()
         }
 
